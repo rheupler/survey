fb5523dc94c9e2746bd9b3abbe151b8150eac484
ryan heupler & ryan broughan <student@epicodus.com>
2015-08-25 16:46:42 -0700

    Add option CRUD and begin quiz routing

--- app.rb
+++ app.rb
@@ -20,9 +20,16 @@ get('/surveys/new') do
   erb(:surveys_new)
 end
 
+get('/questions/:id/edit') do
+  id = params.fetch("id").to_i
+  @question = Question.find(id)
+  erb(:question_edit)
+end
+
 get('/surveys/:id/edit') do
   id = params.fetch("id").to_i
   @survey = Survey.find(id)
+  @ref = params.fetch('ref')
   erb(:survey_edit)
 end
 
@@ -38,7 +45,6 @@ delete ('/surveys/:id') do
   survey = Survey.find(id)
   survey.destroy
   @surveys = Survey.all
-
   erb(:surveys)
 end
 
@@ -59,20 +65,97 @@ delete ('/questions/:id') do
   erb(:survey_detail)
 end
 
+patch('/questions/:id') do
+  id = params.fetch('id').to_i
+  @question = Question.find(id)
+  question_name = params.fetch "question_name"
+  @question.update({name: question_name})
+  @options = @question.options
+  erb(:question_detail)
+end
+
+
 patch('/surveys/:id') do
+  ref = params.fetch('ref')
   id = params.fetch('id').to_i
   @survey = Survey.find(id)
   survey_name = params.fetch 'survey_name'
   @survey.update({name: survey_name})
   @questions = @survey.questions
-  erb :survey_detail
+  @surveys = Survey.all
+  if ref == "detail"
+    erb :survey_detail
+  elsif ref == "surveys"
+    erb :surveys
+  else
+    erb :surveys
+  end
 end
 
-post ('/questions') do
+post('/questions') do
   name = params.fetch "question_name"
   survey_id = params.fetch("survey_id").to_i
   Question.create({name: name, survey_id: survey_id})
   @survey = Survey.find(survey_id)
   @questions = @survey.questions
   erb :survey_detail
+end
+
+get('/questions/:id') do
+  id = params.fetch('id').to_i
+  # question_id = params.fetch('question_id').to_i
+  @question = Question.find(id)
+  @options = @question.options
+  erb(:question_detail)
+end
+
+post('/options') do
+  name = params.fetch 'option_name'
+  question_id = params.fetch('question_id').to_i
+  Option.create({name: name, question_id: question_id})
+  @question = Question.find(question_id)
+  @options = @question.options
+  erb(:question_detail)
+end
+
+patch('/options/:id') do
+  id = params.fetch('id').to_i
+  @option = Option.find(id)
+  option_name = params.fetch 'option_name'
+  @option.update ({name: option_name})
+  @question = @option.question
+  @options = @question.options
+  @option = Option.all
+  erb(:question_detail)
+end
+
+get('/options/:id/edit') do
+  id = params.fetch('id').to_i
+  @option = Option.find(id)
+  erb(:option_edit)
+end
+
+get('/questions/:id/responses/new') do
+
+  id = params.fetch('id').to_i
+  @question = Question.find(id)
+  erb :response_new
+end
+
+post('/responses') do
+  option_id = params.fetch('option').to_i
+  question_id = params.fetch('question_id').to_i
+  question = Question.find(question_id)
+  survey = question.survey
+  response = Response.create({question_id: question_id, option_id: option_id})
+  erb :response_new
+end
+
+delete('/options/:id') do
+  id = params.fetch('id').to_i
+  option = Option.find(id)
+  @question = option.question
+  option.destroy
+  @options = @question.options
+  erb(:question_detail)
 end
--- lib/question.rb
+++ lib/question.rb
@@ -2,4 +2,19 @@ class Question < ActiveRecord::Base
   belongs_to(:survey)
   has_many(:responses)
   has_many(:options)
+
+
+  validates(:name, presence: true)
+  before_save(:trim)
+  before_save(:question_case)
+
+private
+  def trim
+    self.name = self.name.strip
+  end
+
+  def question_case
+    self.name = self.name.downcase
+    self.name = self.name.capitalize
+  end
 end
